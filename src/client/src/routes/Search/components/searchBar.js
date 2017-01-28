c6b6ceaabe39494b7a7c14fd7bde45c0ddb8eaa7
Aquib Master <aquibmaster@gmail.com>
2017-01-28 19:26:07 +1300

    Add functional search bar component.
    + Ignore .DS_Store cause osx :hammer:
    + Add temporary state variable in searchView to display what was searched for

---
+++ src/client/src/routes/Search/components/searchBar.js
@@ -0,0 +1,75 @@
+import React, { PropTypes } from 'react';
+
+/**
+ * Component to handle user search input.
+ */
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { query: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.clearQuery = this.clearQuery.bind(this);
+    }
+
+    /**
+     * Callback to handle change events from the search input.
+     * @param  {Event} event
+     * @return {void}
+     */
+    handleChange(event) {
+        this.setState({query: event.target.value});
+    }
+
+    /**
+     * Callback to handle keypress events from the search input.
+     * @param  {Event} event
+     * @return {void}
+     */
+    handleKeyPress(event) {
+        if(event.key === 'Enter') {
+            this.props.onSearch(this.state.query);
+            event.target.blur();
+        }
+    }
+
+    /**
+     * Mutates state to clear the search query.
+     * @return {void}
+     */
+    clearQuery() {
+        this.setState({query: ''});
+        this.searchInput.focus();
+    }
+
+    /**
+     * Paints the search bar component.
+     * @return {void}
+     */
+    render() {
+        return (
+            <div className="search-bar">
+                <input 
+                    ref={(input) => { this.searchInput = input; }}
+                    type="text"
+                    placeholder="Search your local database..."
+                    className="search-bar__input"
+                    value={this.state.query}
+                    onChange={this.handleChange}
+                    onKeyPress={this.handleKeyPress}
+                    autoFocus />
+
+                { this.state.query &&
+                    <a onClick={this.clearQuery} className="search-bar__clear-btn">x</a>
+                }
+            </div>
+        );
+    }
+}
+
+SearchBar.propTypes = {
+    onSearch: PropTypes.func.isRequired
+};
+
+export default SearchBar;
--- src/client/src/routes/Search/components/searchView.js
+++ src/client/src/routes/Search/components/searchView.js
@@ -1,12 +1,52 @@
 import React from 'react';
+
+import SearchBar from './searchBar';
 import './searchView.scss';
 
-export const SearchView = () => {
-    return (
-        <div>
-            Hello from Search!
-        </div>
-    );
-};
+/**
+ * Responsible for painting and handling interactions with the search page.
+ */
+class SearchView extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.onSearch = this.onSearch.bind(this);
+        this.state = {
+            searchQuery: ''
+        };
+    }
+
+    /**
+     * Callback responsible for handling search.
+     * @param  {string} query The query to search for.
+     * @return {void}
+     */
+    onSearch(query) {
+        this.setState({searchQuery: query});
+    }
+
+    /**
+     * Renders the search view.
+     */
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-8">
+                        <SearchBar onSearch={this.onSearch}  />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-8">
+                        {this.state.searchQuery && 
+                            <p>You searched for {this.state.searchQuery}</p>
+                        }
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default SearchView;
