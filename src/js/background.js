7bc991ba9083250fc8f61fc43f00283771963197
gastonche <gastonnkh@gmail.com>
2017-03-17 09:02:48 +0100

    [background] added listener fr html5 web app url changes done with history api
    [background] added listener fr html5 web app url changes done with history api

--- src/js/background.js
+++ src/js/background.js
@@ -379,3 +379,8 @@ function binarySearch(arr, value, lt, gt, i, j) {
 }
 
 init();
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details){
+    if(details.frameId == 0)
+        chrome.tabs.executeScript(details.tabId, { file: '/js/content.js'});
+});
